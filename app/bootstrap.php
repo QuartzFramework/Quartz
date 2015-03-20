c6a95bcd806bfc7045d5100fe0dbcfdccfbb1508
Matti van de Weem <mattiweem@hotmail.com>
2015-03-20 12:45:06 +0100

    Remove testing code in live version of Quartz

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -6,22 +6,6 @@ require_once('settings.php');
 require_once('app.php');
 $app = new app;
 
-/*
-class  test extends \app{
-
-
-	public function __construct(){
-
-	}
-	public function scoper(){
-		return $this;
-	}
-
-}
-
-$test = new test();
-*/
-
 // require everything from composer here
 require_once(__DIR__.'/../vendor/autoload.php');
 
